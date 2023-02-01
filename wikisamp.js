6b6f4b0b84ae4353781e3f73e476fed15b6e3514
akshayj123 <93759330+akshayj123@users.noreply.github.com>
2023-02-01 13:30:13 +0530

    Create wikisamp.js

---
+++ wikisamp.js
@@ -0,0 +1,5 @@
+const randomPageBtn = document.getElementById("randomPageBtn");
+
+randomPageBtn.addEventListener("click", function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Special:Random";
+});
